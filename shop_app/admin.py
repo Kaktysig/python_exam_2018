36e1e0775ff8f2505fe9dc5b64369ef607619735
kaktyshiny <kaktyshiny@gmail.com>
2018-01-18 10:42:26 +0300

    Adding models and admin

--- shop_app/admin.py
+++ shop_app/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Goods_Variation, Good, Order, Category, Cart
+
+
+class Admin_Category(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ['name',]
+
+
+class Admin_Good(admin.ModelAdmin):
+    list_display = ('name', 'category',)
+    search_fields = ['name', 'category',]
+
+
+class Admin_Goods_Variation(admin.ModelAdmin):
+    list_display = ('__str__', 'parent', 'variation_name', 'cost',)
+    search_fields = ['variation_name', 'get_parent_name', 'cost',]
+
+    def get_parent_name(self, obj):
+        return obj.parent.name
+
+
+class Admin_Cart(admin.ModelAdmin):
+    list_display = ('customer', 'good_count',)
+    search_fields = ['customer',]
+
+    def good_count(self, obj):
+        return obj.goods.count()
+
+    good_count.short_description = "Count of goods"
+
+
+class Admin_Order(admin.ModelAdmin):
+    list_display = ()
+
+admin.site.register(Category, Admin_Category)
+admin.site.register(Good, Admin_Good)
+admin.site.register(Goods_Variation, Admin_Goods_Variation)
+admin.site.register(Cart, Admin_Cart)
+admin.site.register(Order)
--- shop_app/models.py
+++ shop_app/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=140, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Good(models.Model):
+
+    name = models.CharField(max_length=140)
+
+    category = models.ForeignKey('Category', related_name='goods')
+
+    def __str__(self):
+        return self.name
+
+
+class Goods_Variation(models.Model):
+
+    parent = models.ForeignKey('Good', related_name="good")
+
+    variation_name = models.CharField(max_length=140)
+    description = models.TextField(blank=True)
+
+    cost = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return self.parent.name + " " + self.variation_name
+
+
+class Cart(models.Model):
+
+    customer = models.ForeignKey(User, related_name="cart")
+
+    goods = models.ManyToManyField('Goods_Variation', related_name="in_cart")
+
+
+class Order(models.Model):
+
+    customer = models.ForeignKey(User, related_name="order")
+
+    address = models.CharField(max_length=150)
+    datetime = models.DateField()
+
+    goods = models.ForeignKey('Cart', related_name="ordered")
