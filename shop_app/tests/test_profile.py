85a3cbade7fe18367aa8feee4d7e3c1a6a686771
kaktyshiny <kaktyshiny@gmail.com>
2018-01-18 16:27:25 +0300

    Я хочу плакать дважды

--- shop_app/tests/test_profile.py
+++ shop_app/tests/test_profile.py
@@ -3,16 +3,16 @@
 import pytest
 
 
-# @pytest.mark.acceptance
-# def test_create_new_profile(client):
-#     response = client.post('/api/v1/profile/', {
-#         "username": "TestName",
-#         "email": "test@user.ru",
-#         "password": "testpassword",
-#         "cart": ""
-#     })
-#     assert response.status_code == 201
-#
+@pytest.mark.acceptance
+@pytest.mark.django_db
+def test_create_new_profile(client):
+    response = client.post('/api/v1/profile/', {
+    "email": "some@mail.ru",
+    "username": "useruser",
+    "password": "qweqwe",
+})
+    assert response.status_code == 201
+
 
 # @pytest.mark.acceptance
 # def test_login(client):
